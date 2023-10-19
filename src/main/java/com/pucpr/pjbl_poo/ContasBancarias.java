91632acd73c2d0f5f67c680ae45143690d03769c
SirBerns <bernardo.jakubiak@gmail.com>
2023-10-19 20:15:30 -0300

    secondbranch

---
+++ src/main/java/com/pucpr/pjbl_poo/ContasBancarias.java
@@ -0,0 +1,18 @@
+package com.pucpr.pjbl_poo;
+
+abstract public class ContasBancarias {
+    protected int numConta;
+    protected double saldoConta;
+    protected String nomeConta;
+    protected String proprietarioConta;
+
+    public ContasBancarias(int numConta, double saldoConta, String nomeConta, String proprietarioConta){
+        this.numConta = numConta;
+        this.saldoConta = saldoConta;
+        this.nomeConta = nomeConta;
+        this.proprietarioConta = proprietarioConta;
+
+    }
+
+    public abstract double getSaldo();
+}
