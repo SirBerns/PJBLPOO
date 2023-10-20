bed21a9b1d096c015a3f989bd4e7e8dfd63a961b
Dirceucheliga <135722080+Dirceucheliga@users.noreply.github.com>
2023-10-19 21:00:33 -0300

    corrente e poupança

---
+++ PJBL_POO/src/main/java/com/pucpr/pjbl_poo/ContasPoupanca.java
@@ -0,0 +1,16 @@
+package com.pucpr.pjbl_poo;
+
+public class ContasPoupanca extends ContasBancarias {
+
+    public ContasPoupanca(int numConta, double saldoConta, String nomeConta, String proprietarioConta) {
+        super(numConta, saldoConta, nomeConta, proprietarioConta);
+    }
+
+    @Override
+    public double getSaldo(){
+        return this.saldoConta;
+    }
+
+    public void addFees(){       
+    };
+}
