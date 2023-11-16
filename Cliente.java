4bd1495d9f34e53edcfafd0fa4f2201e3dbc865d
Bernardo Jakubiak <115375156+SirBerns@users.noreply.github.com>
2023-11-16 11:26:11 -0300

    Serializacao
    persistencia

--- Cliente.java
+++ Cliente.java
@@ -1,6 +1,6 @@
-package com.pucpr.pjbl_poo;
+import java.io.*;
 
-public class Cliente extends ContasBancarias {
+public class Cliente extends ContasBancarias implements Serializable {
     private final String nome;
     private final int cpf;
     private final int cep;
@@ -11,14 +11,29 @@ public class Cliente extends ContasBancarias {
         this.cpf = cpf;
         this.cep = cep;
     }
-    
-    
-    public void addConta() {
-    };
 
     public double getSaldo(){
         return this.saldoConta;
     }
 
+    // Método para serializar o objeto
+    public void serializarCliente(String nomeArquivo) {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(nomeArquivo))) {
+            outputStream.writeObject(this);
+            System.out.println("Objeto Cliente serializado com sucesso.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-}
+    // Método para desserializar o objeto
+    public static Cliente desserializarCliente(String nomeArquivo) {
+        Cliente cliente = null;
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(nomeArquivo))) {
+            cliente = (Cliente) inputStream.readObject();
+            System.out.println("Objeto Cliente desserializado com sucesso.");
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return cliente;
+    }
